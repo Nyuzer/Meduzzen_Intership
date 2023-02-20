f161c1ba61f7758c68d4fcef253fc84cfe769389
Nyuzer <toporovskiy.nikita@gmail.com>
2023-02-20 18:14:01 +0100

    *All structure

---
+++ project/main.py
@@ -0,0 +1,15 @@
+# import os
+# from dotenv import load_dotenv
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def health_check():
+    return {
+        "status_code": 200,
+        "detail": "ok",
+        "result": "working"
+    }
