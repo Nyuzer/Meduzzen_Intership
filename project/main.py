c68c6e5e7a45d5074dcb0fce0486693b23ac6d6c
Nyuzer <toporovskiy.nikita@gmail.com>
2023-02-22 21:32:17 +0100

    Add CORS, docker-compose.yml and configurate postgres, redis

--- project/main.py
+++ project/main.py
@@ -2,12 +2,26 @@ import os
 from dotenv import load_dotenv
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # add environment variables
 load_dotenv('../venv/.env')
 
 app = FastAPI()
 
+# add CORS
+origins = [
+    "http://0.0.0.0:8000",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=origins,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.get('/')
 def health_check():
