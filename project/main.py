cfe53979fe48d4bddad747f99342a8d5df353737
Nyuzer <toporovskiy.nikita@gmail.com>
2023-02-23 01:06:26 +0100

    Trying to connect to the db, error so far

--- project/main.py
+++ project/main.py
@@ -1,11 +1,19 @@
 import os
+
 from dotenv import load_dotenv
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import databases
+import aioredis
+
 # add environment variables
-load_dotenv('../venv/.env')
+load_dotenv('.env')
+
+DATABASE_URL = os.getenv('DATABASE_URI')
+
+database = databases.Database(DATABASE_URL)
 
 app = FastAPI()
 
@@ -22,6 +30,26 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+
+# Error
+# socket.gaierror: [Errno 8] nodename nor servname provided, or not known
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+# Error
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+# Error
+@app.on_event("startup")
+async def startup_redis():
+    redis = await aioredis.from_url('redis://redis')
+
 
 @app.get('/')
 def health_check():
