9f5cd3572f2192ec1ce5f8d1ef3407bc41c87e82
Nyuzer <toporovskiy.nikita@gmail.com>
2023-02-28 01:03:37 +0100

    Added models and schemas. Problems with alembic

--- project/db/db.py
+++ project/db/db.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .connections import DATABASE_URL
+from project.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_DB, DATABASE_URI
 
-# # Make sense to refactor it and put in core package
 
-SQLALCHEMY_DATABASE_URL = DATABASE_URL
+# Make sense to refactor it and put in core package
+
+SQLALCHEMY_DATABASE_URL = DATABASE_URI
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
